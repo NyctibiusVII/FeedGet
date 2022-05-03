c39a9c888eb282c0b2561decba54cfde1ae9ef97
NyctibiusVII <matheus.dev.07@gmail.com>
2022-05-03 17:25:10 -0300

    :bug: fix: Fixing warnings

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -55,7 +55,7 @@ export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested, 
                 <textarea
                     onChange={e => setComment(e.target.value)}
                     placeholder='Conte com detalhes o que está acontecendo...'
-                    className='md:min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin'
+                    className='md:min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin'
                 />
 
                 <footer className='flex gap-2 mt-2'>
