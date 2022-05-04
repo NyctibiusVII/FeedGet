ce6fe31ea9ad1cf987e1f0548fafaa85f6dd9485
NyctibiusVII <matheus.dev.07@gmail.com>
2022-05-04 16:51:00 -0300

    :wrench: build: 'Prisma' settings

---
+++ server/prisma/migrations/20220504194106_create_feedbacks/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "feedbacks" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "type" TEXT NOT NULL,
+    "comment" TEXT NOT NULL,
+    "screenshot" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
