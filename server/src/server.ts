937c846632c1c8f14f41122f51b54c2a479ea2d5
NyctibiusVII <matheus.dev.07@gmail.com>
2022-05-04 17:14:26 -0300

    :sparkles: feat: Feedback route

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,25 @@
 import express from 'express'
+import { prisma } from './prisma'
 
+// Configuration server
 const app = express()
 
-app.get('/', (req, res) => {
-    return res.send('Hello World!')
+app.use(express.json())
+
+// Routes
+app.post('/feedbacks', async (req, res) => {
+    const { type, comment, screenshot } = req.body
+
+    const feedback = await prisma.feedback.create({
+        data: {
+            type,
+            comment,
+            screenshot
+        }
+    })
+
+    return res.status(201).json({ data: feedback })
 })
 
+// Start server
 app.listen(3333, () => console.log('Server started'))
