b5c15a5b09b7326e093ccf10bac3153282a3f8fc
NyctibiusVII <matheus.dev.07@gmail.com>
2022-05-04 17:40:33 -0300

    :sparkles: feat: Configured email sending via (mailtrap/nodemailer)

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,21 @@
 import express from 'express'
+import nodemailer from 'nodemailer'
+
 import { prisma } from './prisma'
 
 // Configuration server
 const app = express()
 
 app.use(express.json())
+
+const transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+        user: "8f066be777a002",
+        pass: "3b08a7bf7b97ff"
+    }
+})
 
 // Routes
 app.post('/feedbacks', async (req, res) => {
@@ -16,6 +27,20 @@ app.post('/feedbacks', async (req, res) => {
             comment,
             screenshot
         }
+    })
+
+    await transport.sendMail({
+        from: 'Equipe FeedGet <oi@feedget.com>',
+        to: 'Matheus Vidigal <matheus.dev.07@gmail.com>',
+        subject: `Novo feedback de ${type}`,
+        html: [
+            `<div style="font-family: sans-serif; font-size: 16px; color: #272727;">`,
+                `<h1>Feedback</h1>`,
+                `<p>Tipo: ${type}</p>`,
+                `<p>Comentário: ${comment}</p>`,
+                screenshot && `<p>Screenshot: ${screenshot}</p>`,
+            `</div>`,
+        ].join('\n')
     })
 
     return res.status(201).json({ data: feedback })
