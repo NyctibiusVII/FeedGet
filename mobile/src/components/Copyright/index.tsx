1606e2c72b5a008b84bebe9033ced83241539846
NyctibiusVII <matheus.dev.07@gmail.com>
2022-05-05 19:40:41 -0300

    :sparkles: feat: Copyright component

---
+++ mobile/src/components/Copyright/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import {
+    View,
+    Text
+} from 'react-native'
+
+import { styles } from './styles'
+
+export const Copyright = () => <View><Text style={styles.text}>Feito com ♥ pela Rocketseat</Text></View>
