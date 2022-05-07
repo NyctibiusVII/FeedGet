785a8ab04fc25a9c74bb2a310c7b395b2f88f107
NyctibiusVII <matheus.dev.07@gmail.com>
2022-05-06 21:08:08 -0300

    🔧 chore: Changing table field

--- server/prisma/migrations/20220504194106_create_feedbacks/migration.sql
+++ server/prisma/migrations/20220504194106_create_feedbacks/migration.sql
@@ -3,6 +3,5 @@ CREATE TABLE "feedbacks" (
     "id" TEXT NOT NULL PRIMARY KEY,
     "type" TEXT NOT NULL,
     "comment" TEXT NOT NULL,
-    "screenshot" TEXT,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+    "screenshot" TEXT
 );
